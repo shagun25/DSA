f09894e389263dcd49c91cd5ad6bd0f6f60b2063
shagungoyal <goyalshagun25@gmail.com>
2020-11-25 22:04:19 +0530

    added

---
+++ Hashing/chaining.java
@@ -0,0 +1,42 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+class MyHash {
+    int BUCKET;
+    ArrayList<LinkedList<Integer>> table;
+    MyHash(int b)
+    {
+        BUCKET = b;
+        table = new ArrayList<LinkedList<Integer>>();
+        for(int i=0;i<b;i++)
+        table.add(new LinkedList<Integer>());
+    }
+    void insert(Integer k)
+    {
+      int i = k % BUCKET;
+      table.get(i).add(k);
+    }
+    void delete(Integer k)
+    {
+        int i = k % BUCKET;
+        table.get(i).remove(k);
+    }
+    boolean search(Integer k)
+    {
+         int i = k % BUCKET;
+         return table.get(i).contains(k);
+    }
+}
+class GFG {
+public static void main(String[] args) 
+	{ 
+	    MyHash mh = new MyHash(7);
+	    mh.insert(10);
+	    mh.insert(20);
+	    mh.insert(15);
+	    mh.insert(7);
+	    System.out.println(mh.search(10));
+	    mh.delete(15);
+	    System.out.println(mh.search(15));
+	} 
+}
